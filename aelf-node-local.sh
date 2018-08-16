63afabcbcd2ccd11b1384ccfd713d86ceea5f9ec
Eric.Shu <blackeye286@126.com>
2018-08-16 11:42:01 +0800

    update local scripts

--- aelf-node-local.sh
+++ aelf-node-local.sh
@@ -19,7 +19,6 @@ then
 	echo ">>Pull and build aelf files"
 	cd $BaseDir/github/AElf
 	sudo git pull
-	sudo git pull
 	sudo dotnet publish -nowarn:CS0108,CS0162,CS0168,CS0169,CS0219,CS0414,CS0649,CS0659,CS1998,CS2002,CS4014,NU1603,NU1701,MSB3245,MSB3026,xUnit1013,xUnit2000,xUnit2000,xUnit2002,xUnit2009,xUnit2013,xUnit2017 --configuration Release -o $BaseDir/github/AElfRelease
 else
 	echo "No code updated."
@@ -56,13 +55,12 @@ read -p "Select run type(main=1, other=2):" RunType
 if [ $RunType = 1 ]
 then
 	echo "Run MAIN node with command:"
-	echo "sudo dotnet AElf.Launcher.dll --mine.enable true --rpc.port 8000 --rpc.host 0.0.0.0 --db.type redis --db.host 127.0.0.1 --db.port 6379 --db.number 0 --node.account $ACCOUNT --node.port 6800 --dpos.generator true --chain.new true"
-	sudo dotnet AElf.Launcher.dll --mine.enable true --rpc.port 8000 --rpc.host 0.0.0.0 --db.type redis --db.host 127.0.0.1 --db.port 6379 --db.number 0 --node.account $ACCOUNT --node.port 6800 --dpos.generator true --chain.new true
+	echo "sudo dotnet AElf.Launcher.dll --mine.enable true --rpc.port 8000 --rpc.host 0.0.0.0 --db.type redis --db.host 127.0.0.1 --db.port 6379 --db.number 0 --node.account $ACCOUNT --node.accountpassword 123 --node.port 6800 --dpos.generator true --chain.new true"
+	sudo dotnet AElf.Launcher.dll --mine.enable true --rpc.port 8000 --rpc.host 0.0.0.0 --db.type redis --db.host 127.0.0.1 --db.port 6379 --db.number 0 --node.account $ACCOUNT --node.accountpassword 123 --node.port 6800 --dpos.generator true --chain.new true
 else
 	echo "Run OTHER node with command:"
 	read -p "Input main chain chain id: " ChainID
 	sudo sh -c "echo '{\n \"id\":\"$ChainID\"\n}' > $BaseDir/github/AElfRelease/ChainInfo.json"
 	read -p "Input main node rpc info like(192.168.197.34:6800):" MainNode 
-	echo "sudo dotnet AElf.Launcher.dll --mine.enable true --rpc.port 8000 --rpc.host 0.0.0.0 --db.type redis --db.host 127.0.0.1 --db.port 6379 --db.number 0 --node.account $ACCOUNT --node.port 6800 --bootnodes $MainNode"
-	sudo dotnet AElf.Launcher.dll --mine.enable true --rpc.port 8000 --rpc.host 0.0.0.0 --db.type redis --db.host 127.0.0.1 --db.port 6379 --db.number 0 --node.account $ACCOUNT --node.port 6800 --bootnodes $MainNode
-fi
+	echo "sudo dotnet AElf.Launcher.dll --mine.enable true --rpc.port 8000 --rpc.host 0.0.0.0 --db.type redis --db.host 127.0.0.1 --db.port 6379 --db.number 0 --node.account $ACCOUNT --node.accountpassword 123 --node.port 6800 --bootnodes $MainNode"
+	sudo dotnet AElf.Launcher.dll --mine.enable true --rpc.port 8000 --rpc.host 0.0.0.0 --db.type redis --db.host 127.0.0.1 --db.port 6379 --db.number 0 --node.account $ACCOUNT --node.accountpassword 123 --node.port 6800 --bootnodes $MainNode
