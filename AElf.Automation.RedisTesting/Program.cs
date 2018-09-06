1b9104bc129f1390d3ca9cfc5dd854b33fb5fce1
Eric Shu <blackeye286@126.com>
2018-09-06 11:21:40 +0800

    Update redis db testing

--- AElf.Automation.RedisTesting/Program.cs
+++ AElf.Automation.RedisTesting/Program.cs
@@ -50,9 +50,16 @@ namespace AElf.Automation.RedisTesting
 
             var rh = new RedisHelper(redishost);
             var ktm = new KeyTypeManager(rh);
+
+            List<Task> blockTasks = new List<Task>();
+            //Redis Info analyze
+            blockTasks.Add(Task.Run(() =>
+            {
+                ktm.GetAllKeyInfoCollection();
+                ktm.PrintSummaryInfo(false);
+            }));
             //Get Block info
             Logger.WriteInfo("Get current block height information, current total height: {0}.", height);
-            List<Task> blockTasks = new List<Task>();
             int reqHeight = 0;
             object obj = new object();
             for (int i = 0; i < 8; i++)
@@ -83,12 +90,7 @@ namespace AElf.Automation.RedisTesting
                     }
                 }));
             }
-
             Task.WaitAll(blockTasks.ToArray<Task>());
-
-            //Redis Info analyze
-            ktm.GetAllKeyInfoCollection();
-            ktm.PrintSummaryInfo(false);
 
             Logger.WriteInfo("Begin print block info by height");
             Logger.WriteInfo("-------------------------------------------------------------------------------------------------------------");
@@ -100,83 +102,97 @@ namespace AElf.Automation.RedisTesting
                 var j = i;
                 contractTasks.Add(Task.Run(() =>
                 {
-                    BlockInfo block;
+
                     while (true)
                     {
                         try
                         {
+                            BlockInfo block = null;
                             if (!BlockCollection.TryDequeue(out block))
-                            break;
-
-                        Logger.WriteInfo($"Block Height: {block.Height}, TxCount:{block.Transactions.Count}");
-                        //Analyze Blockhash
-                        var keyinfoList = ktm.HashList["Hash"]
-                            .FindAll(o => o.ValueInfo.ToString().Contains(block.BlockHash));
-                        if (keyinfoList != null && keyinfoList?.Count !=0)
-                        {
-                            foreach (var keyinfo in keyinfoList)
+                                break;
+                            Logger.WriteInfo("BlockCollection analyze left: {0}", BlockCollection.Count);
+                            Logger.WriteInfo($"Block Height: {block.Height}, TxCount:{block.Transactions.Count}");
+                            List<Task> analyzeTasks = new List<Task>();
+                            //Analyze Block Hash
+                            analyzeTasks.Add(Task.Run(() =>
                             {
-                                keyinfo.Checked = true;
-                                Logger.WriteInfo(keyinfo.ToString());
-                                if (keyinfo.HashString == "Chain")
+                                var keyinfoList = ktm.HashList["Hash"]
+                                    .FindAll(o => o.ValueInfo.ToString().Contains(block.BlockHash));
+                                if (keyinfoList != null && keyinfoList?.Count != 0)
                                 {
-                                    var hash = new AElf.Kernel.Hash(keyinfo.KeyObject.Value);
-                                    string hashValue = hash.ToHex();
-                                    var changeInfo = ktm.HashList["Hash"]
-                                        .FirstOrDefault(o => o.ValueInfo.ToString().Contains(hashValue));
-                                    if (changeInfo != null)
+                                    foreach (var keyinfo in keyinfoList)
                                     {
-                                        changeInfo.Checked = true;
-                                        Logger.WriteInfo(changeInfo.ToString());
+                                        keyinfo.Checked = true;
+                                        Logger.WriteInfo(keyinfo.ToString());
+                                        if (keyinfo.HashString == "Chain")
+                                        {
+                                            var hash = new AElf.Kernel.Hash(keyinfo.KeyObject.Value);
+                                            string hashValue = hash.ToHex();
+                                            var changeInfo = ktm.HashList["Hash"]
+                                                .FirstOrDefault(o => o.ValueInfo.ToString().Contains(hashValue));
+                                            if (changeInfo != null)
+                                            {
+                                                changeInfo.Checked = true;
+                                                Logger.WriteInfo(changeInfo.ToString());
+                                            }
+                                        }
                                     }
                                 }
-                            }
-                        }
+                            }));
 
-                        var blockBody = ktm.HashList["BlockBody"]
-                            .FirstOrDefault(o => o.ValueInfo.ToString().Contains(block.BlockHash));
-                        if (blockBody != null)
-                        {
-                            blockBody.Checked = true;
-                            Logger.WriteInfo(blockBody.ToString());
-                        }
-
-                        //Analyze PreviousBlockHash
-                        //Analyze MerkleTreeRootOfTransactions
-                        //Analyze MerkleTreeRootOfWorldState
-                        var blockHeader = ktm.HashList["BlockHeader"]
-                            .FirstOrDefault(o => o.ValueInfo.ToString().Contains(block.PreviousBlockHash));
-                        if (blockHeader != null)
-                        {
-                            blockHeader.Checked = true;
-                            Logger.WriteInfo(blockHeader.ToString());
-                        }
-
-                        //Analyze Transactions
-                        foreach (var transaction in block.Transactions)
-                        {
-                            //Transaction
-                            var txResult = ra.GetTxResult(transaction.Trim());
-                            string incrementId = txResult["result"]["result"]["tx_info"]["IncrementId"].ToString();
-                            string checkStr = $"\"IncrementId\": \"{incrementId}\"";
-                            var transactionInfo = ktm.HashList["Transaction"]
-                                .FirstOrDefault(o => o.ValueInfo.ToString().Contains(checkStr));
-                            if (transactionInfo != null)
+                            //Analyze BlockBody
+                            analyzeTasks.Add(Task.Run(() =>
                             {
-                                transactionInfo.Checked = true;
-                                Logger.WriteInfo(transactionInfo.ToString());
-                            }
-
-                            //Transaction Result
-                            var transactionResult = ktm.HashList["TransactionResult"]
-                                .FirstOrDefault(o => o.ValueInfo.ToString().Contains(transaction.Trim()));
-
-                                if (transactionResult != null)
+                                var blockBody = ktm.HashList["BlockBody"]
+                                    .FirstOrDefault(o => o.ValueInfo.ToString().Contains(block.BlockHash));
+                                if (blockBody != null)
                                 {
-                                    transactionResult.Checked = true;
-                                    Logger.WriteInfo(transactionResult.ToString());
+                                    blockBody.Checked = true;
+                                    Logger.WriteInfo(blockBody.ToString());
                                 }
-                            }
+                            }));
+
+                            //Analyze PreviousBlockHash
+                            analyzeTasks.Add(Task.Run(() =>
+                            {
+                                var blockHeader = ktm.HashList["BlockHeader"]
+                                    .FirstOrDefault(o => o.ValueInfo.ToString().Contains(block.PreviousBlockHash));
+                                if (blockHeader != null)
+                                {
+                                    blockHeader.Checked = true;
+                                    Logger.WriteInfo(blockHeader.ToString());
+                                }
+                            }));
+
+                            //Analyze Transactions
+                            analyzeTasks.Add(Task.Run(() =>
+                            {
+                                foreach (var transaction in block.Transactions)
+                                {
+                                    //Transaction
+                                    var txResult = ra.GetTxResult(transaction.Trim());
+                                    string incrementId = txResult["result"]["result"]["tx_info"]["IncrementId"].ToString();
+                                    string checkStr = $"\"IncrementId\": \"{incrementId}\"";
+                                    var transactionInfo = ktm.HashList["Transaction"]
+                                        .FirstOrDefault(o => o.ValueInfo.ToString().Contains(checkStr));
+                                    if (transactionInfo != null)
+                                    {
+                                        transactionInfo.Checked = true;
+                                        Logger.WriteInfo(transactionInfo.ToString());
+                                    }
+
+                                    //Transaction Result
+                                    var transactionResult = ktm.HashList["TransactionResult"]
+                                        .FirstOrDefault(o => o.ValueInfo.ToString().Contains(transaction.Trim()));
+
+                                    if (transactionResult != null)
+                                    {
+                                        transactionResult.Checked = true;
+                                        Logger.WriteInfo(transactionResult.ToString());
+                                    }
+                                }
+                            }));
+                            Task.WaitAll(analyzeTasks.ToArray<Task>());
                         }
                         catch (Exception e)
                         {
