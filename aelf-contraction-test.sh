c5184ac818d20b79521e2cd38548d5d8f297dba2
Eric Shu <blackeye286@126.com>
2018-10-22 18:46:24 +0800

    Add automation testing script.

---
+++ aelf-contraction-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo mkdir -p /home/aelf/github/AutomationRelease
+
+read -p "Update Testing code or not(yes/no): UPDATE
+if [ $UPDATE == "yes" ]
+then
+    echo ">Update AElf code"
+    cd /home/aelf/github/aelf-automation-test/AElf
+    sudo rm -rf $(sudo rm -rf ./ -name *.g.cs)
+    sudo git pull
+    echo ">Update Test code"
+    cd /home/aelf/github/aelf-automation-test/AElf.Automation.RpcPerformance
+    sudo git pull
+    echo ">Build Test code"
+    sudo dotnet build --configuration Release -o /home/aelf/github/AutomationRelease
+fi
+
+echo "Run automation testing."
+cd /home/aelf/github/AutomationRelease
+read -p "Input testing parameters(like: 4 10 http://192.168.197.34:8000/chain):" PARAMETER
+sudo dotnet AElf.Automation.RpcPerformance.dll $PARAMETER
