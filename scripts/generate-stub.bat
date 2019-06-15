5b319e79ded372040cf5f67be3b6f25bfe9e16e3
EricShu <blackeye286@126.com>
2019-06-15 23:36:04 +0800

    update build on windows

--- scripts/generate-stub.bat
+++ scripts/generate-stub.bat
@@ -3,7 +3,7 @@ SET scriptdir=%~dp0
 call "%scriptdir%download_binary.bat"
 
 protoc --proto_path=../AElf/protobuf ^
---csharp_out=internal_access:./Protobuf\Generated ^
+--csharp_out=./Protobuf\Generated ^
 --csharp_opt=file_extension=.g.cs ^
 --contract_opt=stub ^
 --contract_out=./Protobuf/Generated ^
