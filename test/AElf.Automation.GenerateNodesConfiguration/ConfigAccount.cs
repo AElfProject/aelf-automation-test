9abddffb7378d8d9fcb5b600a2ff759cdec50e2f
EricShu <blackeye286@126.com>
2019-07-19 19:25:27 +0800

    update config multiple nodes

---
+++ test/AElf.Automation.GenerateNodesConfiguration/ConfigAccount.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using AElf.Automation.Common.Helpers;
+using AElf.Automation.Common.OptionManagers;
+using AElf.Types;
+using Volo.Abp.Threading;
+
+namespace AElf.Automation.GenerateNodesConfiguration
+{
+    public class ConfigAccount
+    {
+        private string _keyPath;
+        private IKeyStore _keyStore;
+        private NodeOption _node;
+        public ConfigAccount(NodeOption node)
+        {
+            var dataPath = CommonHelper.GetCurrentDataDir();
+            _keyPath = Path.Combine(dataPath, "keys");
+            _keyStore = new AElfKeyStore(dataPath);
+            _node = node;
+        }
+
+        public string GenerateAccount()
+        {
+            var keypair = AsyncHelper.RunSync(() => _keyStore.CreateAsync("123", "AELF"));
+            var pubKey = keypair.PublicKey;
+            _node.PublicKey = pubKey.ToHex();
+
+            var addr = Address.FromPublicKey(pubKey);
+            _node.Account = addr.GetFormatted();
+            
+            return _node.Account;
+        }
+
+        public void CopyAccount()
+        {
+            var originPath = Path.Combine(_keyPath, $"{_node.Account}.ak");
+            var desPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "results", _node.Name);
+            CommonHelper.CopyFiles(originPath, desPath);
+        }
+    }
+}
