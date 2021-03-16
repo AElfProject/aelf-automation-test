9c3f0ae71d1c49e688c323e4b577a227ceab41a6
YueZh127 <yue.zheng@aelf.io>
2021-03-16 17:03:50 +0800

    fix qps

--- test/AElf.Automation.AccountCheck/Program.cs
+++ test/AElf.Automation.AccountCheck/Program.cs
@@ -15,7 +15,7 @@ namespace AElf.Automation.AccountCheck
     {
         static void Main(string[] args)
         {
-            Log4NetHelper.LogInit("AccountCheck_");
+            Log4NetHelper.LogInit("AccountCheck");
 
             var transfer = new TransferAction();
             var check = new CheckAction();
@@ -35,25 +35,29 @@ namespace AElf.Automation.AccountCheck
                 {
                     foreach (var contract in transfer.ContractInfos)
                     {
-                        var token = new TokenContract(transfer.NodeManager, transfer.InitAccount, contract.ContractAddress);
+                        var token = new TokenContract(transfer.NodeManager, transfer.InitAccount,
+                            contract.ContractAddress);
                         _contractList.Add(token);
                         _tokenInfoList.Add(token, contract.TokenSymbol);
                     }
                 }
-                
+
                 transfer.PrepareTransfer(_tokenInfoList);
-                _fromAccountInfos = check.CheckBalance(check.FromAccountList, _tokenInfoList,out long d1);
-                _toAccountInfos = check.CheckBalance(check.ToAccountList, _tokenInfoList,out long d2);
+                // original balance 
+                _fromAccountInfos = check.CheckBalance(check.FromAccountList, _tokenInfoList, out long d1);
+                _toAccountInfos = check.CheckBalance(check.ToAccountList, _tokenInfoList, out long d2);
                 long all = 0;
                 while (times > 0)
                 {
                     Logger.Info($"{times}");
                     transfer.Transfer(_tokenInfoList);
-                    var from = check.CheckBalance(check.FromAccountList, _tokenInfoList, out long  fromDuration);
+
+                    //after transfer balance
+                    var from = check.CheckBalance(check.FromAccountList, _tokenInfoList, out long fromDuration);
                     var to = check.CheckBalance(check.ToAccountList, _tokenInfoList, out long toDuration);
-                    
+
                     Logger.Info("Check from account balance:");
-                    foreach (var (symbol,list) in _fromAccountInfos)
+                    foreach (var (symbol, list) in _fromAccountInfos)
                     {
                         var after = from.First(a => a.Key.Equals(symbol));
                         foreach (var account in list)
@@ -62,9 +66,9 @@ namespace AElf.Automation.AccountCheck
                             account.Balance.ShouldBe(accountInfo.Balance + amount);
                         }
                     }
-                
+
                     Logger.Info("Check to account balance:");
-                    foreach (var (symbol,list) in _toAccountInfos)
+                    foreach (var (symbol, list) in _toAccountInfos)
                     {
                         var after = to.First(a => a.Key.Equals(symbol));
                         foreach (var account in list)
@@ -79,7 +83,9 @@ namespace AElf.Automation.AccountCheck
                     _toAccountInfos = to;
                     times--;
                 }
-                var req = (double)(check.CheckTimes * (check.FromAccountList.Count+check.ToAccountList.Count)) / (all / 1000);
+
+                var req = (double) (check.CheckTimes * (check.FromAccountList.Count + check.ToAccountList.Count) *
+                                    _tokenInfoList.Count) / all * 1000;
                 Logger.Info($"1s request {req}");
             }
             else
@@ -92,17 +98,17 @@ namespace AElf.Automation.AccountCheck
                 {
                     Task.Run(() =>
                     {
-                        while (times > 0 )
+                        while (times > 0)
                         {
                             Logger.Info($"{times}");
-                            check.CheckBalanceOnly(check.AccountList,check.ContractInfos, out long duration);
+                            check.CheckBalanceOnly(check.AccountList, check.ContractInfos, out long duration);
                             all += duration;
                             times--;
                         }
                     }, token)
                 };
                 Task.WaitAll(taskList.ToArray<Task>());
-                var req = (double)(check.CheckTimes * check.AccountList.Count) / (all / 1000);
+                var req = (double) (check.CheckTimes * check.AccountList.Count)* check.ContractInfos.Count / all * 1000;
                 Logger.Info($"1s request {req}");
             }
         }
@@ -110,8 +116,7 @@ namespace AElf.Automation.AccountCheck
         private static readonly ILog Logger = Log4NetHelper.GetLogger();
         private static List<TokenContract> _contractList;
         private static Dictionary<TokenContract, string> _tokenInfoList;
-        private static Dictionary<string,List<AccountInfo>> _fromAccountInfos;
-        private static Dictionary<string,List<AccountInfo>> _toAccountInfos;
-
+        private static Dictionary<string, List<AccountInfo>> _fromAccountInfos;
+        private static Dictionary<string, List<AccountInfo>> _toAccountInfos;
     }
 }
