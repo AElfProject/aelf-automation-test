69c637b05909f87e0181c21bd472c59434be44f6
wanghuan-520 <huan.wang@aelf.io>
2022-01-13 16:44:46 +0800

    add NFTMarket test

---
+++ src/AElfChain.Common/Contracts/NFTMarketContract.cs
@@ -0,0 +1,334 @@
+using AElf.Client.Dto;
+using AElf.Client.Proto;
+using AElf.Contracts.MultiToken;
+using AElf.Contracts.NFTMarket;
+using AElfChain.Common.DtoExtension;
+using AElfChain.Common.Managers;
+using Google.Protobuf.WellKnownTypes;
+using AddressList = AElf.Contracts.NFTMarket.AddressList;
+using StringList = AElf.Contracts.NFTMarket.StringList;
+
+namespace AElfChain.Common.Contracts
+{
+    public enum NFTMarketContractMethod
+    {
+        // For Sellers
+        ListWithFixedPrice,
+        ListWithEnglishAuction,
+        ListWithDutchAuction,
+        Deal,
+        Delist,
+
+        // For Buyers
+        MakeOffer,
+        CancelOffer,
+
+        // For Creators
+        SetRoyalty,
+        SetTokenWhiteList,
+        SetCustomizeInfo,
+        StakeForRequests,
+        WithdrawStakingTokens,
+        HandleRequest,
+
+        // For Admin
+        SetServiceFee,
+        SetGlobalTokenWhiteList,
+
+        //View
+        GetListedNFTInfo,
+        GetWhiteListAddressPriceList,
+        GetOfferAddressList,
+        GetOfferList,
+        GetBidAddressList,
+        GetBidList,
+        GetCustomizeInfo,
+        GetRequestInfo,
+        GetEnglishAuctionInfo,
+        GetDutchAuctionInfo
+    }
+
+    public class NFTMarketContract : BaseContract<NFTMarketContractMethod>
+    {
+        public NFTMarketContract(INodeManager nm, string account) :
+            base(nm, ContractFileName, account)
+        {
+        }
+
+        public NFTMarketContract(INodeManager nm, string callAddress, string contractAbi) :
+            base(nm, contractAbi)
+        {
+            SetAccount(callAddress);
+        }
+
+        public static string ContractFileName => "AElf.Contracts.NFTMarket";
+
+        public TransactionResultDto ListWithFixedPrice(string symbol, long tokenId, Price price, long quantity,
+            ListDuration duration, WhiteListAddressPriceList whiteListAddressPriceList)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.ListWithFixedPrice, new ListWithFixedPriceInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                Price = price,
+                Quantity = quantity,
+                Duration = duration,
+                WhiteListAddressPriceList = whiteListAddressPriceList
+            });
+        }
+
+        public TransactionResultDto ListWithEnglishAuction(string symbol, long tokenId, long startingPrice,
+            string purchaseSymbol,
+            ListDuration duration, WhiteListAddressPriceList whiteListAddressPriceList)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.ListWithEnglishAuction,
+                new ListWithEnglishAuctionInput
+                {
+                    Symbol = symbol,
+                    TokenId = tokenId,
+                    StartingPrice = startingPrice,
+                    PurchaseSymbol = purchaseSymbol,
+                    Duration = duration,
+                    WhiteListAddressPriceList = whiteListAddressPriceList
+                });
+        }
+
+        public TransactionResultDto ListWithDutchAuction(string symbol, long tokenId, long startingPrice,
+            long endingPrice, string purchaseSymbol,
+            ListDuration duration, WhiteListAddressPriceList whiteListAddressPriceList)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.ListWithDutchAuction, new ListWithDutchAuctionInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                StartingPrice = startingPrice,
+                EndingPrice = endingPrice,
+                PurchaseSymbol = purchaseSymbol,
+                Duration = duration,
+                WhiteListAddressPriceList = whiteListAddressPriceList
+            });
+        }
+
+        public TransactionResultDto Deal(string symbol, long tokenId, string offerMaker, Price price, long quantity)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.Deal, new DealInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                OfferMaker = offerMaker.ConvertAddress(),
+                Price = price,
+                Quantity = quantity
+            });
+        }
+
+        public TransactionResultDto Delist(string symbol, long tokenId, long quantity)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.Delist, new DelistInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                Quantity = quantity
+            });
+        }
+
+        public TransactionResultDto MakeOffer(string symbol, long tokenId, string offerTo, long quantity, Price price,
+            Timestamp expireTime, Timestamp dueTime)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.MakeOffer, new MakeOfferInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                OfferTo = offerTo.ConvertAddress(),
+                Quantity = quantity,
+                Price = price,
+                ExpireTime = expireTime,
+                DueTime = dueTime
+            });
+        }
+
+        public TransactionResultDto CancelOffer(string symbol, long tokenId, Int32List indexList, string offerFrom,
+            string offerTo)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.CancelOffer, new CancelOfferInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                IndexList = indexList,
+                OfferFrom = offerFrom.ConvertAddress(),
+                OfferTo = offerTo.ConvertAddress()
+            });
+        }
+
+        public TransactionResultDto SetRoyalty(string symbol, long tokenId, int royalty, string royaltyFeeReceiver)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.SetRoyalty, new SetRoyaltyInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                Royalty = royalty,
+                RoyaltyFeeReceiver = royaltyFeeReceiver.ConvertAddress()
+            });
+        }
+
+        public TransactionResultDto SetTokenWhiteList(string symbol, StringList tokenWhiteList)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.SetTokenWhiteList, new SetTokenWhiteListInput
+            {
+                Symbol = symbol,
+                TokenWhiteList = tokenWhiteList
+            });
+        }
+
+        public TransactionResultDto SetCustomizeInfo(string symbol, int depositRate, Price price, long workHours,
+            long whiteListHours, long stakingAmount)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.SetCustomizeInfo, new CustomizeInfo
+            {
+                Symbol = symbol,
+                DepositRate = depositRate,
+                Price = price,
+                WorkHours = workHours,
+                WhiteListHours = whiteListHours,
+                StakingAmount = stakingAmount
+            });
+        }
+
+        public TransactionResultDto StakeForRequests(string symbol, long stakingAmount)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.StakeForRequests, new StakeForRequestsInput
+            {
+                Symbol = symbol,
+                StakingAmount = stakingAmount
+            });
+        }
+
+        public TransactionResultDto WithdrawStakingTokens(string symbol, long withdrawAmount)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.WithdrawStakingTokens, new WithdrawStakingTokensInput
+            {
+                Symbol = symbol,
+                WithdrawAmount = withdrawAmount
+            });
+        }
+
+        public TransactionResultDto HandleRequest(string symbol, long tokenId, string requester, bool isConfirm)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.HandleRequest, new HandleRequestInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                Requester = requester.ConvertAddress(),
+                IsConfirm = isConfirm
+            });
+        }
+
+        public TransactionResultDto SetServiceFee(int serviceFeeRate, string serviceFeeReceiver)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.SetServiceFee, new SetServiceFeeInput
+            {
+                ServiceFeeRate = serviceFeeRate,
+                ServiceFeeReceiver = serviceFeeReceiver.ConvertAddress()
+            });
+        }
+
+        public TransactionResultDto SetGlobalTokenWhiteList(StringList globalTokenWhiteList)
+        {
+            return ExecuteMethodWithResult(NFTMarketContractMethod.SetGlobalTokenWhiteList, globalTokenWhiteList);
+        }
+
+        public ListedNFTInfo GetListedNFTInfo(string symbol, long tokenId, string owner)
+        {
+            return CallViewMethod<ListedNFTInfo>(NFTMarketContractMethod.GetListedNFTInfo, new GetListedNFTInfoInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                Owner = owner.ConvertAddress()
+            });
+        }
+
+        public WhiteListAddressPriceList GetWhiteListAddressPriceList(string symbol, long tokenId)
+        {
+            return CallViewMethod<WhiteListAddressPriceList>(NFTMarketContractMethod.GetWhiteListAddressPriceList,
+                new GetWhiteListAddressPriceListInput
+                {
+                    Symbol = symbol,
+                    TokenId = tokenId
+                });
+        }
+
+        public AddressList GetOfferAddressList(string symbol, long tokenId)
+        {
+            return CallViewMethod<AddressList>(NFTMarketContractMethod.GetOfferAddressList,
+                new GetOfferAddressListInput
+                {
+                    Symbol = symbol,
+                    TokenId = tokenId
+                });
+        }
+
+        public OfferList GetOfferList(string symbol, long tokenId, string address)
+        {
+            return CallViewMethod<OfferList>(NFTMarketContractMethod.GetOfferList, new GetOfferListInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                Address = address.ConvertAddress()
+            });
+        }
+
+        public AddressList GetBidAddressList(string symbol, long tokenId)
+        {
+            return CallViewMethod<AddressList>(NFTMarketContractMethod.GetBidAddressList,
+                new GetOfferAddressListInput
+                {
+                    Symbol = symbol,
+                    TokenId = tokenId
+                });
+        }
+
+        public OfferList GetBidList(string symbol, long tokenId, string address)
+        {
+            return CallViewMethod<OfferList>(NFTMarketContractMethod.GetBidList, new GetOfferListInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId,
+                Address = address.ConvertAddress()
+            });
+        }
+
+        public CustomizeInfo GetCustomizeInfo(string symbol)
+        {
+            return CallViewMethod<CustomizeInfo>(NFTMarketContractMethod.GetCustomizeInfo,
+                new StringValue {Value = symbol});
+        }
+
+        public RequestInfo GetRequestInfo(string symbol, long tokenId)
+        {
+            return CallViewMethod<RequestInfo>(NFTMarketContractMethod.GetRequestInfo, new GetRequestInfoInput
+            {
+                Symbol = symbol,
+                TokenId = tokenId
+            });
+        }
+
+        public EnglishAuctionInfo GetEnglishAuctionInfo(string symbol, long tokenId)
+        {
+            return CallViewMethod<EnglishAuctionInfo>(NFTMarketContractMethod.GetEnglishAuctionInfo,
+                new GetEnglishAuctionInfoInput
+                {
+                    Symbol = symbol,
+                    TokenId = tokenId
+                });
+        }
+
+        public DutchAuctionInfo GetDutchAuctionInfo(string symbol, long tokenId)
+        {
+            return CallViewMethod<DutchAuctionInfo>(NFTMarketContractMethod.GetDutchAuctionInfo,
+                new GetDutchAuctionInfoInput
+                {
+                    Symbol = symbol,
+                    TokenId = tokenId
+                });
+        }
+    }
+}
---
+++ test/AElf.Automation.FeatureVerification/NFTMarketContractTest.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using AElf.Contracts.NFT;
+using AElf.Contracts.NFTMarket;
+using AElf.Types;
+using AElfChain.Common;
+using AElfChain.Common.Contracts;
+using AElfChain.Common.DtoExtension;
+using AElfChain.Common.Helpers;
+using AElfChain.Common.Managers;
+using Google.Protobuf.WellKnownTypes;
+using log4net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using CreateInput = AElf.Contracts.NFT.CreateInput;
+
+namespace AElf.Automation.Contracts.ScenarioTest
+{
+    [TestClass]
+    public class NFTMarketContractTest
+    {
+        private ILog Logger { get; set; }
+        private INodeManager NodeManager { get; set; }
+        private int _chainId;
+        private GenesisContract _genesisContract;
+        private TokenContract _tokenContract;
+        private NftContract _nftContract;
+        private NFTMarketContract _nftMarketContract;
+
+        private string InitAccount { get; } = "J6zgLjGwd1bxTBpULLXrGVeV74tnS2n74FFJJz7KNdjTYkDF6";
+        // private string InitAccount { get; } = "nn659b9X1BLhnu5RWmEUbuuV7J9QKVVSN54j9UmeCbF3Dve5D";
+        private string OtherAccount { get; } = "sjzNpr5bku3ZyvMqQrXeBkXGEvG2CTLA2cuNDfcDMaPTTAqEy";
+        private string WhiteListAddress1 { get; } = "sjzNpr5bku3ZyvMqQrXeBkXGEvG2CTLA2cuNDfcDMaPTTAqEy";
+        private string WhiteListAddress2 { get; } = "2bs2uYMECtHWjB57RqgqQ3X2LrxgptWHtzCqGEU11y45aWimh4";
+        private string WhiteListAddress3 { get; } = "YUW9zH5GhRboT5JK4vXp5BLAfCDv28rRmTQwo418FuaJmkSg8";
+
+        private static string RpcUrl { get; } = "192.168.66.9:8000";
+        private AuthorityManager AuthorityManager { get; set; }
+
+        private string NFT = "";
+        private string NFTMarket = "";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Log4NetHelper.LogInit("NFTMarketContractTest");
+            Logger = Log4NetHelper.GetLogger();
+            NodeInfoHelper.SetConfig("nodes-env2-main");
+
+            NodeManager = new NodeManager(RpcUrl);
+            AuthorityManager = new AuthorityManager(NodeManager, InitAccount);
+            _chainId = ChainHelper.ConvertBase58ToChainId(NodeManager.GetChainId());
+
+            if (NFT.Equals(""))
+                _nftContract = new NftContract(NodeManager, InitAccount);
+            else
+                _nftContract = new NftContract(NodeManager, InitAccount, NFT);
+
+            if (NFTMarket.Equals(""))
+                _nftMarketContract = new NFTMarketContract(NodeManager, InitAccount);
+            else
+                _nftMarketContract = new NFTMarketContract(NodeManager, InitAccount, NFTMarket);
+
+            _genesisContract = GenesisContract.GetGenesisContract(NodeManager, InitAccount);
+            _tokenContract = _genesisContract.GetTokenContract(InitAccount);
+        }
+
+        [TestMethod]
+        public void ListWithFixedPriceTest()
+        {
+            var tokenId = 0;
+            var totalAmount = 1000;
+            var sellAmount = 100;
+            var fixedPrice = 10;
+            var whitePrice1 = 9;
+            var whitePrice2 = 10;
+            var whitePrice3 = 11;
+            var symbol = CreateAndMint(totalAmount, tokenId);
+            var startTime = DateTime.UtcNow.AddSeconds(10).ToTimestamp();
+            var publicTime = DateTime.UtcNow.AddHours(24).ToTimestamp();
+            var durationHours = 48;
+
+            var listedNFTInfo = _nftMarketContract.GetListedNFTInfo(symbol, tokenId, InitAccount);
+            listedNFTInfo.ShouldBeNull();
+
+            _nftMarketContract.ListWithFixedPrice(
+                symbol,
+                tokenId,
+                new Price
+                {
+                    Symbol = "ELF",
+                    Amount = fixedPrice
+                },
+                sellAmount,
+                new ListDuration
+                {
+                    StartTime = startTime,
+                    PublicTime = publicTime,
+                    DurationHours = durationHours
+                },
+                new WhiteListAddressPriceList()
+            );
+
+            listedNFTInfo = _nftMarketContract.GetListedNFTInfo(symbol, tokenId, InitAccount);
+            listedNFTInfo.Symbol.ShouldBe(symbol);
+            listedNFTInfo.TokenId.ShouldBe(tokenId);
+            listedNFTInfo.Owner.ShouldBe(InitAccount.ConvertAddress());
+            listedNFTInfo.Quantity.ShouldBe(sellAmount);
+            listedNFTInfo.ListType.ShouldBe(ListType.FixedPrice);
+            listedNFTInfo.Price.Symbol.ShouldBe("ELF");
+            listedNFTInfo.Price.Amount.ShouldBe(fixedPrice);
+            listedNFTInfo.Duration.StartTime.ShouldBe(startTime);
+            listedNFTInfo.Duration.PublicTime.ShouldBe(publicTime);
+            listedNFTInfo.Duration.DurationHours.ShouldBe(durationHours);
+        }
+
+        private string CreateAndMint(long amount, long tokenId)
+        {
+            var createResult =
+                _nftContract.ExecuteMethodWithResult(NftContractMethod.Create, new CreateInput
+                {
+                    NftType = NFTType.Collectables.ToString(),
+                    ProtocolName = "CAT",
+                    TotalSupply = 10000,
+                    Creator = InitAccount.ConvertAddress(),
+                    IsBurnable = true,
+                    IssueChainId = _chainId,
+                    Metadata = new Metadata(),
+                    BaseUri = "aelf.com/nft/",
+                    IsTokenIdReuse = true
+                });
+            createResult.Status.ConvertTransactionResultStatus().ShouldBe(TransactionResultStatus.Mined);
+            var symbol = StringValue.Parser.ParseFrom(ByteArrayHelper.HexStringToByteArray(createResult.ReturnValue))
+                .Value;
+            Logger.Info($"symbol is {symbol}");
+
+            var initBalanceBefore = GetBalanceTest(InitAccount, symbol, tokenId);
+            Logger.Info($"initBalanceBefore is {initBalanceBefore}");
+
+            var mintResult =
+                _nftContract.ExecuteMethodWithResult(NftContractMethod.Mint, new MintInput
+                {
+                    Symbol = symbol,
+                    Alias = "NFT_CO_CAT1",
+                    Metadata = new Metadata(),
+                    Quantity = amount,
+                    TokenId = tokenId
+                });
+            mintResult.Status.ConvertTransactionResultStatus().ShouldBe(TransactionResultStatus.Mined);
+
+            var initAccountAfterBalance = GetBalanceTest(InitAccount, symbol, tokenId);
+            Logger.Info($"initAccountAfterBalance is {initAccountAfterBalance}");
+            initAccountAfterBalance.ShouldBe(amount + initBalanceBefore);
+
+            return symbol;
+        }
+
+        private long GetBalanceTest(string owner, string symbol, long tokenId)
+        {
+            var getBalance = _nftContract.GetBalance(owner, symbol, tokenId);
+
+            Logger.Info($"owner of {symbol} is {getBalance.Owner}");
+            Logger.Info($"TokenHash of {symbol} is {getBalance.TokenHash}");
+            Logger.Info($"Balance of {symbol} is {getBalance.Balance}");
+
+            return getBalance.Balance;
+        }
+    }
+}
